9e866721eb9c7ce3bfd0595b3a59c140fee1cecf
carlobortolan <carlobortolan@gmail.com>
2024-02-05 15:42:38 +0100

    Update Rails example

--- ruby_on_rails/app/controllers/welcome_controller.rb
+++ ruby_on_rails/app/controllers/welcome_controller.rb
@@ -6,12 +6,13 @@ class WelcomeController < ApplicationController
   def index
   end
 
+  # Call our API and request application session
   def make_request
     session = {
       mode: "job",
-      job_slug: "job#1",
-      success_url: "mypage.com/success",
-      cancel_url: "mypage.com/failure"
+      job_slug: params[:job_slug],
+      success_url: params[:success_url],
+      cancel_url: params[:cancel_url]
     }
     client = Embloy::Client.new(ENV.fetch('CLIENT_TOKEN'), session)
     redirect_url = client.make_request
