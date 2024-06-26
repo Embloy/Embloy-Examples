13dcc207b1cbd7b94ca6a1694e56be7826c7ba8d
carlobortolan <carlobortolan@gmail.com>
2024-06-26 10:44:10 +0200

    Update rails example

--- ruby_on_rails/app/controllers/welcome_controller.rb
+++ ruby_on_rails/app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
   # Call our API and request application session
   def make_request
     puts "Starting make_request method"
-
+  
     session = {
       mode: "job",
       job_slug: params[:job_slug],
@@ -17,14 +17,19 @@ class WelcomeController < ApplicationController
       cancel_url: params[:cancel_url]
     }
     puts "Session data prepared: #{session.inspect}"
-
+  
     client = Embloy::Client.new(ENV.fetch('CLIENT_TOKEN'), session)
     puts "Embloy client initialized with session data"
-
+  
     redirect_url = client.make_request
-    puts "Received redirect URL from Embloy client: #{redirect_url}"
-
-    redirect_to(redirect_url, allow_other_host: true)
-    puts "Redirecting to: #{redirect_url}"
+    if redirect_url.nil?
+      puts "Error: Received nil redirect URL from Embloy client"
+      # Handle the error, e.g., render an error page or redirect to a fallback URL
+      render plain: "Error: Unable to process your request.", status: :internal_server_error
+    else
+      puts "Received redirect URL from Embloy client: #{redirect_url}"
+      redirect_to(redirect_url, allow_other_host: true)
+      puts "Redirecting to: #{redirect_url}"
+    end
   end
 end
