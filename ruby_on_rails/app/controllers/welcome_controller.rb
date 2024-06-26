02117b78f35b4718990f4444c16e287437264dd0
carlobortolan <carlobortolan@gmail.com>
2024-06-26 10:38:36 +0200

    Update rails example controller

--- ruby_on_rails/app/controllers/welcome_controller.rb
+++ ruby_on_rails/app/controllers/welcome_controller.rb
@@ -8,15 +8,23 @@ class WelcomeController < ApplicationController
 
   # Call our API and request application session
   def make_request
+    puts "Starting make_request method"
+
     session = {
       mode: "job",
       job_slug: params[:job_slug],
       success_url: params[:success_url],
       cancel_url: params[:cancel_url]
     }
+    puts "Session data prepared: #{session.inspect}"
+
     client = Embloy::Client.new(ENV.fetch('CLIENT_TOKEN'), session)
+    puts "Embloy client initialized with session data"
+
     redirect_url = client.make_request
+    puts "Received redirect URL from Embloy client: #{redirect_url}"
 
     redirect_to(redirect_url, allow_other_host: true)
+    puts "Redirecting to: #{redirect_url}"
   end
 end
